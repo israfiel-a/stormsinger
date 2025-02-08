4310ad0cca780e2fdde385ad8274620d032d62d2
Israfil <israfiel_buisness.nearly025@passfwd.com>
2025-02-07 20:08:12 -0800

    removed hideous error callback

--- Lightbleeder/Source/Window.cpp
+++ Lightbleeder/Source/Window.cpp
@@ -7,11 +7,6 @@ namespace Lightbleeder
 {
     Window::Window()
     {
-        glfwSetErrorCallback([](int error, const char *description) {
-            std::cout << "Error " << error << ": " << description
-                      << std::endl;
-        });
-
         bool initialized = glfwInit();
         if (!initialized)
             // TODO: Log system.
