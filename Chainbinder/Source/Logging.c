c4e9d399125bba9300091106e2fca3320d719ea4
Israfil <israfiel_buisness.nearly025@passfwd.com>
2025-03-05 18:10:35 -0800

    fixed format string that gcc was whinging about (%u ->%lu)

--- Chainbinder/Source/Logging.c
+++ Chainbinder/Source/Logging.c
@@ -12,9 +12,8 @@ inline static void LogPrefix(const char *const filename,
                              chainbinder_u16_t line,
                              chainbinder_log_type_t type)
 {
-    fprintf(stdout,
-            "[\033[%hhum%s\033[0m] %-12s @ ln. %.04u: ", color_codes[type],
-            string_descriptors[type], filename, line);
+    fprintf(stdout, "[\033[%hhum%s\033[0m] %-12s @ ln. %.04lu: ",
+            color_codes[type], string_descriptors[type], filename, line);
 }
 
 void _Chainbinder_Log_INTERNAL(const char *const filename,
