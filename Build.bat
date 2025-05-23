8467c4cb5daaf3ff6ad4de054d63912fed65f8b1
Israfil Argos <174603598+israfiel-a@users.noreply.github.com>
2025-05-23 12:45:32 -0700

    removed bat script bc visual studio easier :)

--- Build.bat
+++
@@ -1,8 +0,0 @@
-if [%2]==[] goto normal
-cmake -B build -DCMAKE_BUILD_TYPE=%1 -DSTORMSINGER_DISABLE_ASAN:BOOL=%2
-goto rest
-:normal
-cmake -B build -DCMAKE_BUILD_TYPE=%1
-:rest
-cd build && cmake --build . --parallel 9 --config %1
-cd Stormsinger\Debug && .\Stormsinger.exe
