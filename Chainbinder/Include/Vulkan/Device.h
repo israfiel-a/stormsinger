0ac62babb0f16299b61de0a924b53532a9685be2
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-05-30 22:06:42 -0400

    removed all old content to be rewritten

--- Chainbinder/Include/Vulkan/Device.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHAINBINDER_VULKAN_DEVICE_H
-#define CHAINBINDER_VULKAN_DEVICE_H
-
-#include <Chainbinder.h>
-
-bool Chainbinder_FindPhysicalDevice(void);
-bool Chainbinder_CreateLogicalDevice(void);
-
-#endif // CHAINBINDER_VULKAN_DEVICE_H
